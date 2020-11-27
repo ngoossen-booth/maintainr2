f0159f710d636c7ab3bec519c15937c5d428cd9f
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-11-27 17:48:23 +0000

    more fixes

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,7 @@
 class AboutController < ApplicationController
+    # Uncomment this if you want to force users to sign in before any other actions
+   skip_before_action(:force_user_sign_in, { :only => [:index})
+  
   def index
 
     render({ :template => "about/index.html.erb" })
