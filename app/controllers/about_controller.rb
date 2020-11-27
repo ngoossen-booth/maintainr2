2fdff2094a0805aa1b06d11633f0379f1d095360
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-11-27 17:50:49 +0000

    bug hunting

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,6 @@
 class AboutController < ApplicationController
     # Uncomment this if you want to force users to sign in before any other actions
-   skip_before_action(:force_user_sign_in, { :only => [:index})
+   skip_before_action(:force_user_sign_in, { :only => [:index]})
   
   def index
 
